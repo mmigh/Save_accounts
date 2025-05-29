7225441c3cec755413088961b1436e5f6c6a6c54
Primal_han <hieu30671@gmail.com>
2025-05-29 16:19:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -39,8 +39,9 @@ def read_logcals():
     return logcals
 
 def save_logcal(logcal_json):
+    safe_json = json.dumps(json.loads(logcal_json), ensure_ascii=False)
     last_row = len(sheet.get_all_values()) + 1
-    sheet.insert_row(["", "", logcal_json], last_row)
+    sheet.insert_row(["", "", safe_json], last_row)
     
 def delete_logcal(logcal_json):
     cell = sheet.find(logcal_json)
